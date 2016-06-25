613d188d4e570c4f0017a40de3beb204a29e45cd
Daniel Selsam <daniel.selsam@gmail.com>
2016-06-25 14:32:08 -0700

    feat(init/meta/tactic): mk_eq_simp_ext helper

--- library/init/meta/tactic.lean
+++ library/init/meta/tactic.lean
@@ -272,6 +272,7 @@ target >>= defeq_simp >>= change
 
 -- TODO(Leo): remove unifier.conservative after we finish new elaborator
 set_option unifier.conservative true
+
 meta_definition simp : tactic unit :=
 do gs ← get_goals,
    match gs with
@@ -289,6 +290,20 @@ do gs ← get_goals,
      set_goals (new_g :: rest)
    | _              := fail "simp called but no goals"
    end
+
+meta_definition mk_eq_simp_ext (simp_ext : expr → tactic (prod expr expr)) : tactic unit :=
+do gs ← get_goals,
+   match gs with
+   | [g] := do tgt ← target,
+               match expr.is_eq tgt with
+               | option.none := fail "simplifier extension expects a goal of the form [ctx |- l = ?r]"
+               | option.some (start, res) := do r ← simp_ext start,
+                                                unify res (prod.pr1 r),
+                                                unify g (prod.pr2 r)
+               end
+   | _ := fail "simplifier extension expects a goal of the form [ctx |- l = ?r]"
+   end
+
 
 /- Return the number of goals that need to be solved -/
 meta_definition num_goals     : tactic nat :=
