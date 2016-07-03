b59512f6c67ab8c4090b19df18bc35615ca00e0b
Daniel Selsam <daniel.selsam@gmail.com>
2016-07-03 02:10:53 +0100

    okay fixed bug

--- tests/lean/run/simp3.lean
+++ tests/lean/run/simp3.lean
@@ -5,7 +5,7 @@ attribute A_cr [instance]
 open tactic
 open simplifier.unit simplifier.ac simplifier.distrib
 
---set_option trace.simplifier.canonicalize true
+set_option trace.simplifier.canonicalize true
 --set_option trace.defeq_exhaustive_canonicalizer.canonicalize true
 
 set_option pp.all true
@@ -13,15 +13,18 @@ set_option pp.all true
 --example : x + y = y + x := by simp >> triv
 --example : x + y + z = z + y + x := by simp >> triv
 --example : (x + y) * z = x * z + y * z := by simp >> triv
---example : (x + y) * (z + w) = x * z + x * w + y * z + y * w := by simp >> triv
+example : (x + y) * (z + w) = x * z + x * w + y * z + y * w := by simp >> triv
 
-example : (x + y) * (z + w) = w * x + w * y + y * z + x * z := by simp >> triv
+--example : (x + y) * (z + w) = w * x + w * y + y * z + x * z := by simp >> triv
 
 
 --example : (x + y) * (z + w) = x * z + x * w + y * z + y * w * 1 + 0 :=
 --by simp >> trace_state >> triv
+
 /-
-example : @distrib.to_has_mul.{1} A (@ring.to_distrib.{1} A (@comm_ring.to_ring.{1} A A_cr)) = (@comm_semigroup._trans_of_to_semigroup.{1} A (@comm_ring.to_comm_semigroup.{1} A A_cr)) := rfl
+example : @distrib.to_has_mul.{1} A (@ring.to_distrib.{1} A (@comm_ring.to_ring.{1} A A_cr)) = (@comm_semigroup._trans_of_to_semigroup.{1} A (@comm_ring.to_comm_semigroup.{1} A A_cr)) :=
+by dsimp >> trace "state:" >> trace_state >> reflexivity
+--by simp >> trace "state:" >> trace_state >> triv
 -/
 
 --check @distrib.to_has_mul.{1} A (@ring.to_distrib.{1} A (@comm_ring.to_ring.{1} A A_cr))
