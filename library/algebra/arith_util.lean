643ff92b78da167dc565e4f1a347437f95171a75
Daniel Selsam <daniel.selsam@gmail.com>
2016-07-13 11:14:04 -0700

    pos nums in cyclic numerals

--- library/algebra/arith_util.lean
+++ library/algebra/arith_util.lean
@@ -15,9 +15,13 @@ constants (real.of_rat : rat → real)
 constants (rat.of_int : int → rat)
 constants (int.of_nat : nat → int)
 
-definition from_nat (A : Type) [has_zero A] [has_one A] [has_add A] : ℕ → A := sorry
+definition from_pos_num (A : Type) [has_zero A] [has_one A] [has_add A] : pos_num → A
+| (pos_num.one) := 1
+| (pos_num.bit0 k) := bit0 (from_pos_num k)
+| (pos_num.bit1 k) := bit1 (from_pos_num k)
+
 structure cyclic_numerals [class] (A : Type) extends comm_ring A :=
-(bound : ℕ)  (cyclic : ∀ a k : A , a = a - k * from_nat A bound)
+(bound : pos_num)  (cyclic : ∀ a k : A , a = a - k * from_pos_num A bound)
 
 constant (bv2 : Type.{1})
 constant (bv2_cr : field bv2)
