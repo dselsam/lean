287cea0a4fa94b4f93581f45c5cc269e641c15db
Daniel Selsam <daniel.selsam@gmail.com>
2016-07-13 11:07:48 -0700

    from_nat cyclic numerals

--- library/algebra/arith_util.lean
+++ library/algebra/arith_util.lean
@@ -15,23 +15,9 @@ constants (real.of_rat : rat → real)
 constants (rat.of_int : int → rat)
 constants (int.of_nat : nat → int)
 
---attribute real.of_rat [coercion]
---attribute rat.of_int [coercion]
---attribute int.of_nat [coercion]
-
--- TODO(dhs): this may not be the best way to do this
-
---structure has_numerals [class] (A : Type) extends has_zero A, has_one A, has_add A := (from_nat : ℕ → A)
-/-
 definition from_nat (A : Type) [has_zero A] [has_one A] [has_add A] : ℕ → A := sorry
-
 structure cyclic_numerals [class] (A : Type) extends comm_ring A :=
 (bound : ℕ)  (cyclic : ∀ a k : A , a = a - k * from_nat A bound)
--/
-
--- Note: I would rather it extend a comm_ring but it triggers a bunch of errors
-structure cyclic_numerals [class] (A : Type) extends comm_ring A :=
-(bound : A)  (cyclic : ∀ a k : A , a = a - k * bound)
 
 constant (bv2 : Type.{1})
 constant (bv2_cr : field bv2)
