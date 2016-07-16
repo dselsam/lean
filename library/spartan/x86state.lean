e2265272ce9eb81209dc5fb2b868c2291358b8f1
Daniel Selsam <daniel.selsam@gmail.com>
2016-07-16 11:35:05 -0700

    kind of a mess

--- library/spartan/x86state.lean
+++ library/spartan/x86state.lean
@@ -9,7 +9,6 @@ attribute uint32_in [instance]
 constant (map : Π (A B : Type.{1}) [decidable_eq A] [inhabited A], Type.{1})
 namespace map
 constants (insert : Π {A B : Type.{1}} [decidable_eq A] [inhabited A], A → B → map A B → map A B)
---constants (contains : Π {A B : Type.{1}} [decidable_eq A], A → map A B → Prop)
 constants (lookup : Π {A B : Type.{1}} [decidable_eq A] [inhabited A], A → map A B → B)
 
 end map
@@ -33,7 +32,7 @@ definition frame := map uint32 uint32
 
 namespace x86
 structure state := (regs : map x86reg uint32)
-                   (stack : prod frame (list frame)) -- really just want non-empty subtype
+                   (stack : prod frame (list frame)) -- really just want non-empty list subtype
                    (heap : map uint32 uint32)
 
 end x86
@@ -51,8 +50,8 @@ HS.bind get (λ s, match s with x86.state.mk regs stack heap := HS.return stack 
 definition get_stack_frame : code frame :=
 HS.bind get_stack (λ stack, match stack with (fr, rest) := HS.return fr end)
 
-definition get_heap : code (uint32 → uint32) :=
-HS.bind get (λ s, match s with x86.state.mk regs stack heap := HS.return heap end)
+definition get_heap : code (map uint32 uint32) :=
+HS.bind get (λ s, match s with (x86.state.mk regs stack heap) := HS.return heap end)
 
 ----------------------------
 
@@ -79,9 +78,8 @@ definition get_stack_addr (k : uint32) : code uint32 :=
 definition eval_operand : operand → code uint32
 | (operand.const u) := HS.return u
 | (operand.reg r) := get_reg r
+| (operand.stack saddr) := get_stack_addr saddr
 | (operand.heap addr) := get_mem_addr addr
-| (operand.stack k) := get_stack_addr k
-
 
 definition put_reg (v : uint32) (reg : x86reg) : code unit :=
   modify (λ s, match s with
@@ -96,3 +94,49 @@ definition put_mem_addr (v : uint32) : maddr → code unit
                                 match s with
                                 | (x86.state.mk regs stack heap) := x86.state.mk regs stack (map.insert (addr + offset) v heap)
                                 end))
+
+definition put_stack_addr (v : uint32) (saddr : uint32) : code unit :=
+  modify (λ s, match s with
+               | (x86.state.mk regs (fr, rest) heap) := x86.state.mk regs (map.insert saddr v fr, rest) heap
+               end)
+
+definition store_at_operand (v : uint32) : operand → code unit
+| (operand.const u) := put_mem_addr v (maddr.const u)
+| (operand.reg r) := put_reg v r
+| (operand.stack saddr) := put_stack_addr v saddr
+| (operand.heap addr) := put_mem_addr v addr
+
+-- Instructions
+-- The abstract "instruction" datatype will denote to these
+
+namespace x86
+
+definition mov (dst src : operand) : code unit :=
+  HS.bind (eval_operand src) (λ v, store_at_operand v dst)
+
+definition add (dst src : operand) : code unit :=
+HS.bind (eval_operand src)
+        (λ increment, HS.bind
+                      (eval_operand dst)
+                      (λ old_value, store_at_operand (old_value + increment) dst))
+
+end x86
+
+namespace test
+
+definition incr (op : operand) : code unit := x86.add op (operand.const 1)
+
+lemma incr_actually_increments (s s' : x86.state) (op : operand) (pre_s : HoareState.pre (incr op) s) :
+  HoareState.post (incr op) s unit.star s' →
+    eval_state (eval_operand op) s' sorry = eval_state (eval_operand op) s sorry + 1
+      := sorry
+
+lemma incr_arbitrary_requires_ensures (s s' : x86.state) (op : operand) :
+      HoareState.pre (incr op) s →
+      eval_state (eval_operand op) s sorry = 10 → -- (arbitrary requires)
+      /- (extra requires) ... → -/
+      HoareState.post (incr op) s unit.star s' →
+      eval_state (eval_operand op) s' sorry = 11 -- (arbitrary ensures)
+      /- ∧ ... (extra ensures) ... -/ := sorry
+
+end test
