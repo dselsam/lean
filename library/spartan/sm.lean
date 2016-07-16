23a18f698c26f01d3525ec9a6a9f783044122cc3
Daniel Selsam <daniel.selsam@gmail.com>
2016-07-16 15:46:26 -0700

    non-hoare state monad

---
+++ library/spartan/sm.lean
@@ -0,0 +1,112 @@
+namespace monad
+namespace state
+
+definition State (S A : Type) := S → prod A S
+variable {S : Type}
+namespace state
+
+definition fmap : Π {A B : Type}, (A → B) → State S A → State S B :=
+  λ (A B : Type) (f : A → B) (P : S → prod A S) (s : S),
+     match P s with
+     | (a, s') := (f a, s')
+     end
+
+definition ret : Π {A : Type}, A → State S A :=
+  λ (A : Type) (a : A) (s : S), (a, s)
+
+definition bind : Π {A B : Type}, State S A → (A → State S B) → State S B :=
+  λ (A B : Type) (P₁ : State S A) (P₂ : A → State S B) (s₁ : S),
+    match P₁ s₁ with
+    | (a, s₂) := P₂ a s₂
+    end
+end state
+
+definition is_monad [instance] : monad (State S) :=
+monad.mk (@state.fmap S) (@state.ret S) (@state.bind S)
+
+definition get : State S S := λ (s : S), (s, s)
+definition put (s_new : S) : State S unit := λ (s : S), (unit.star, s_new)
+definition modify (f : S → S) : State S unit := state.bind get (λ s', put (f s')) -- elaborator is nuts
+definition run_state {A : Type} (P : State S A) (s : S) : prod A S := P s
+definition eval_state {A : Type} (P : State S A) (s₁ : S) : A := match P s₁ with
+                                                                | (a, s₂) := a
+                                                                end
+
+definition exec_state {A : Type} (P : State S A) (s₁ : S) : S := match P s₁ with
+                                                                | (a, s₂) := s₂
+                                                                end
+
+end state
+end monad
+open monad.state
+
+namespace hoare
+variable {S : Type}
+
+definition Pre := S → Prop
+definition Post (A : Type) := S → A → S → Prop
+
+definition HoareTriple {A : Type} (pre : Pre) (P : State S A) (post : Post A) :=
+∀ s, pre s → match run_state P s with
+               | (a, s') := post s a s'
+               end
+
+lemma strengthen_PRE {A : Type} {pre₁ pre₂ : Pre} {P : State S A} {post : Post A} :
+  HoareTriple pre₁ P post → (∀ s, pre₂ s → pre₁ s) → HoareTriple pre₂ P post :=
+assume H₁ : HoareTriple pre₁ P post,
+assume H₂ : ∀ s, pre₂ s → pre₁ s,
+assume (s : S) (p₂ : pre₂ s),
+H₁ s (H₂ s p₂)
+
+lemma weaken_POST {A : Type} {pre : Pre} {P : State S A} {post₁ post₂ : Post A} :
+  HoareTriple pre P post₁ → (∀ s a s', post₁ s a s' → post₂ s a s') → HoareTriple pre P post₂ :=
+assume H₁ : HoareTriple pre P post₁,
+assume H₂ : ∀ {s} {a} {s'}, post₁ s a s' → post₂ s a s',
+assume (s : S) (p : pre s),
+@prod.cases_on _ _ (λ pair, match pair with (a, s') := post₁ s a s' end → match pair with (a, s') := post₂ s a s' end)
+  (run_state P s)
+  (λ a s', assume HP₁, H₂ HP₁)
+  (H₁ s p)
+
+lemma bind_SPEC {A B : Type} :
+                ∀ (P₁ : State S A) (P₂ : A → State S B)
+                {pre₁ : Pre} {pre₂ : A → Pre}
+                {post₁ : Post A} {post₂ : A → Post B},
+                HoareTriple pre₁ P₁ post₁ →
+                (∀ a, HoareTriple (pre₂ a) (P₂ a) (post₂ a)) →
+                HoareTriple (λ s₁, pre₁ s₁ ∧ (∀ a s₂, post₁ s₁ a s₂ → pre₂ a s₂))
+                            (state.bind P₁ P₂)
+                            (λ s₁ b s₃, ∃ a s₂, post₁ s₁ a s₂ ∧ post₂ a s₂ b s₃) := sorry
+
+lemma get_SPEC : HoareTriple (λ (s : S), true) get (λ s a s', s' = s ∧ a = s) :=
+assume (s : S) (ignore : true),
+and.intro rfl rfl
+
+lemma put_SPEC (s₀ : S) : HoareTriple (λ (s : S), true) (put s₀) (λ s a s', s' = s₀) :=
+assume (s : S) (ignore : true),
+rfl
+
+lemma modify_SPEC (f : S → S) : HoareTriple (λ (s : S), true) (modify f) (λ s a s', s' = f s) :=
+assume (s : S) (ignore : true),
+rfl
+
+lemma modify_SPEC_composition (f : S → S) : HoareTriple (λ (s : S), true) (state.bind get (λ s', put (f s'))) (λ s a s', s' = f s) :=
+have H₁ : HoareTriple (λ (s : S), true) get (λ s a s', s' = s ∧ a = s), from get_SPEC,
+have H₂ : ∀ (a : S), HoareTriple (λ (s : S), true) (put (f a)) (λ s b s', s' = f a), from assume a, put_SPEC (f a),
+have H₃ : HoareTriple (λ (s₁ : S), true ∧ (∀ a s₂, s₂ = s₁ ∧ a = s₁ → true))
+                      (state.bind get (λ s', put (f s')))
+                      (λ s₁ b s₃, ∃ a s₂, (s₂ = s₁ ∧ a = s₁) ∧ s₃ = (f s₁)), from bind_SPEC get (λ s', put (f s')) H₁ H₂,
+have H₄ : HoareTriple (λ (s₁ : S), true)
+                      (state.bind get (λ s', put (f s')))
+                      (λ s₁ b s₃, ∃ a s₂, (s₂ = s₁ ∧ a = s₁) ∧ s₃ = (f s₁)),
+     from strengthen_PRE H₃ (λ s ig₁, and.intro trivial (λ ig₁ ig₂ ig₃, trivial)),
+have H₅ : HoareTriple (λ (s₁ : S), true)
+                      (state.bind get (λ s', put (f s')))
+                      (λ s₁ b s₃, s₃ = f s₁),
+     from weaken_POST H₄ (λ s ig₁ s' ex₁,
+                         Exists.cases_on ex₁
+                           (λ a₁ ex₂, Exists.cases_on ex₂ (λ a₂ and₁, and.elim_right and₁))),
+H₅
+
+-- The overall strategy would be: see what PRE and POST the bind version would yield, and then strengthen and weaken.
+end hoare
---
+++ library/spartan/x86state_sm.lean
@@ -0,0 +1,135 @@
+import .sm
+import algebra.ring
+
+open monad.state
+
+constants (uint32 : Type.{1}) (uint32_cr : comm_ring uint32) (uint32_de : decidable_eq uint32) (uint32_in : inhabited uint32)
+attribute uint32_cr [instance]
+attribute uint32_de [instance]
+attribute uint32_in [instance]
+
+constant (map : Π (A B : Type.{1}) [decidable_eq A] [inhabited A], Type.{1})
+namespace map
+constants (insert : Π {A B : Type.{1}} [decidable_eq A] [inhabited A], A → B → map A B → map A B)
+constants (lookup : Π {A B : Type.{1}} [decidable_eq A] [inhabited A], A → map A B → B)
+
+end map
+
+
+inductive x86reg : Type.{1} := eax | ebx | edx
+definition x86reg_dec_eq [instance] : decidable_eq x86reg := sorry -- need 'deriving ...'!
+definition x86reg_inhabited [instance] : inhabited x86reg := sorry -- need 'deriving ...'!
+
+inductive maddr : Type.{1} :=
+| const : uint32 → maddr
+| reg : x86reg → uint32 → maddr
+
+inductive operand : Type.{1} :=
+| const : uint32 → operand
+| reg : x86reg → operand
+| heap : maddr → operand
+| stack : uint32 → operand
+
+definition operand_dec_eq [instance] : decidable_eq operand := sorry
+
+definition frame := map uint32 uint32
+
+namespace x86
+structure state := (regs : map x86reg uint32)
+                   (stack : prod frame (list frame)) -- really just want non-empty list subtype
+                   (heap : map uint32 uint32)
+
+end x86
+
+definition code [reducible] (A : Type) := State x86.state A
+
+definition get_regs : code (map x86reg uint32) :=
+state.bind get (λ s, match s with x86.state.mk regs stack heap := state.ret regs end)
+
+definition get_stack : code (prod frame (list frame)) :=
+state.bind get (λ s, match s with x86.state.mk regs stack heap := state.ret stack end)
+
+definition get_stack_frame : code frame :=
+state.bind get_stack (λ stack, match stack with (fr, rest) := state.ret fr end)
+
+definition get_heap : code (map uint32 uint32) :=
+state.bind get (λ s, match s with (x86.state.mk regs stack heap) := state.ret heap end)
+
+----------------------------
+
+definition get_reg (reg : x86reg) : code uint32 :=
+  state.bind get_regs (λ regs, state.ret $ map.lookup reg regs)
+
+definition get_mem_addr : maddr → code uint32
+| (maddr.const u) := state.ret u
+| (maddr.reg reg offset) := state.bind (get_reg reg) (λ addr, state.ret $ addr + offset)
+
+definition get_stack_addr (k : uint32) : code uint32 :=
+  state.bind get_stack_frame (λ fr, state.ret $ map.lookup k fr)
+
+definition eval_operand : operand → code uint32
+| (operand.const u) := state.ret u
+| (operand.reg r) := get_reg r
+| (operand.stack saddr) := get_stack_addr saddr
+| (operand.heap addr) := get_mem_addr addr
+
+definition put_reg (v : uint32) (reg : x86reg) : code unit :=
+  modify (λ s, match s with
+               | (x86.state.mk regs stack heap) := x86.state.mk (map.insert reg v regs) stack heap
+               end)
+
+definition put_mem_addr (v : uint32) : maddr → code unit
+| (maddr.const u) := modify (λ s, match s with
+                                  | (x86.state.mk regs stack heap) := x86.state.mk regs stack (map.insert u v heap)
+                                  end)
+| (maddr.reg reg offset) := state.bind (get_reg reg) (λ addr, modify (λ s,
+                                match s with
+                                | (x86.state.mk regs stack heap) := x86.state.mk regs stack (map.insert (addr + offset) v heap)
+                                end))
+
+definition put_stack_addr (v : uint32) (saddr : uint32) : code unit :=
+  modify (λ s, match s with
+               | (x86.state.mk regs (fr, rest) heap) := x86.state.mk regs (map.insert saddr v fr, rest) heap
+               end)
+
+definition store_at_operand (v : uint32) : operand → code unit
+| (operand.const u) := put_mem_addr v (maddr.const u)
+| (operand.reg r) := put_reg v r
+| (operand.stack saddr) := put_stack_addr v saddr
+| (operand.heap addr) := put_mem_addr v addr
+
+-- Instructions
+-- The abstract "instruction" datatype will denote to these
+
+namespace x86
+
+definition mov (dst src : operand) : code unit :=
+  state.bind (eval_operand src) (λ v, store_at_operand v dst)
+
+definition add (dst src : operand) : code unit :=
+state.bind (eval_operand src)
+        (λ increment, state.bind
+                      (eval_operand dst)
+                      (λ old_value, store_at_operand (old_value + increment) dst))
+
+end x86
+
+namespace test
+open hoare
+
+definition incr (op : operand) : code unit := x86.add op (operand.const 1)
+
+lemma incr_actually_increments (s : x86.state) (op : operand) :
+  HoareTriple (λ s, true) (incr op)
+    (λ s a s',
+    eval_state (eval_operand op) s' = eval_state (eval_operand op) s + 1
+    ∧ ∀ op', op' ≠ op → eval_state (eval_operand op') s' = eval_state (eval_operand op') s) := sorry
+
+lemma incr_actually_increments_alt (s : x86.state) (op : operand) :
+  HoareTriple (λ s, true) (incr op)
+    (λ s a s',
+    ∀ op', eval_state (eval_operand op) s' =
+      if op' = op then eval_state (eval_operand op) s + 1
+                  else eval_state (eval_operand op) s) := sorry
+
+end test
