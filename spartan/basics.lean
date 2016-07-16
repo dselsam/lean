ec09fb618515b598040fc06475891e27347ede4d
Daniel Selsam <daniel.selsam@gmail.com>
2016-07-15 20:38:16 -0700

    minor

--- spartan/basics.lean
+++ spartan/basics.lean
@@ -32,6 +32,14 @@ monad.mk (@state.fmap S) (@state.ret S) (@state.bind S)
 definition get : State S S := λ (s : S), (s, s)
 definition put (s_new : S) : State S unit := λ (s : S), (unit.star, s_new)
 definition modify (f : S → S) : State S unit := state.bind get put -- elaborator is nuts
+definition run_state {A : Type} (P : State S A) (s : S) : prod A S := P s
+definition eval_state {A : Type} (P : State S A) (s₁ : S) : A := match P s₁ with
+                                                                | (a, s₂) := a
+                                                                end
+
+definition exec_state {A : Type} (P : State S A) (s₁ : S) : S := match P s₁ with
+                                                                | (a, s₂) := s₂
+                                                                end
 
 end state
 end monad
@@ -43,7 +51,7 @@ open monad.state
 
 namespace spartan
 
-inductive x64reg : Type.{1} := eax | ebx
+inductive x64reg : Type.{1} := eax | ebx | edx
 definition x64reg_dec_eq [instance] : decidable_eq x64reg := sorry
 
 inductive maddr : Type.{1} :=
@@ -113,3 +121,21 @@ state.bind
 (eval_operand dst) (λ old_value, store_at_operand (old_value + increment) dst))
 
 end spartan
+
+open spartan
+
+namespace test
+definition incr (op : operand) : code unit := add op (operand.const 1)
+definition wrap_incr (op : operand) (k : uint32) : code unit :=
+state.bind (incr op)
+(λ ignore₁, state.bind (add op (operand.const k))
+(λ ignore₂, mov (operand.reg x64reg.edx) op))
+
+lemma incr_correct : ∀ (s : state) (op : operand),
+  eval_state (eval_operand op) (exec_state (incr op) s) == eval_state (eval_operand op) s + 1 := sorry
+
+-- TODO(dhs): hoare state monad!
+
+
+
+end test
