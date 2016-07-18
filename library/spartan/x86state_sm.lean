e9d1d59881964bcb6a7179bdb89994f1bd7b2793
Daniel Selsam <daniel.selsam@gmail.com>
2016-07-18 14:03:23 -0700

    minor

--- library/spartan/x86state_sm.lean
+++ library/spartan/x86state_sm.lean
@@ -4,11 +4,13 @@ import algebra.ring
 open monad.state
 
 constants (uint32 : Type.{1}) (uint32_cr : comm_ring uint32) (uint32_de : decidable_eq uint32) (uint32_in : inhabited uint32)
+          (bw_and bw_or bw_not bw_xor bw_ror32 : uint32 → uint32 → uint32) (bw_not32 : uint32 → uint32)
 attribute uint32_cr [instance]
 attribute uint32_de [instance]
 attribute uint32_in [instance]
 
 constant (map : Π (A B : Type.{1}) [decidable_eq A] [inhabited A], Type.{1})
+
 namespace map
 constants (insert : Π {A B : Type.{1}} [decidable_eq A] [inhabited A], A → B → map A B → map A B)
 constants (lookup : Π {A B : Type.{1}} [decidable_eq A] [inhabited A], A → map A B → B)
@@ -16,7 +18,7 @@ constants (lookup : Π {A B : Type.{1}} [decidable_eq A] [inhabited A], A → ma
 end map
 
 
-inductive x86reg : Type.{1} := eax | ebx | edx
+inductive x86reg : Type.{1} := eax | ebx | ecx | edx | edi
 definition x86reg_dec_eq [instance] : decidable_eq x86reg := sorry -- need 'deriving ...'!
 definition x86reg_inhabited [instance] : inhabited x86reg := sorry -- need 'deriving ...'!
 
@@ -106,12 +108,22 @@ namespace x86
 definition mov (dst src : operand) : code unit :=
   state.bind (eval_operand src) (λ v, store_at_operand v dst)
 
-definition add (dst src : operand) : code unit :=
-state.bind (eval_operand src)
-        (λ increment, state.bind
-                      (eval_operand dst)
-                      (λ old_value, store_at_operand (old_value + increment) dst))
+definition update_bin (f : uint32 → uint32 → uint32) (dst src : operand) : code unit :=
+do increment ← eval_operand src,
+   old ← eval_operand dst,
+   store_at_operand (f old increment) dst
 
+definition update_un (f : uint32 → uint32) (dst : operand) : code unit :=
+do old ← eval_operand dst,
+   store_at_operand (f old) dst
+
+definition add := update_bin add
+definition and := update_bin bw_and
+definition or := update_bin bw_or
+definition xor := update_bin bw_xor
+definition ror32 := update_bin bw_ror32
+
+definition not32 := update_un bw_not32
 end x86
 
 namespace test
@@ -133,3 +145,82 @@ lemma incr_actually_increments_alt (s : x86.state) (op : operand) :
                   else eval_state (eval_operand op) s) := sorry
 
 end test
+
+namespace test_sha256
+
+notation x `:==` y := x86.mov x y
+notation x `+:=` y := x86.add x y
+notation x `&:=` y := x86.and x y
+notation x `|:=` y := x86.or x y
+notation x `^:=` y := x86.xor x y
+notation `#` k := operand.stack k
+
+attribute operand.const [coercion]
+attribute operand.reg [coercion]
+open x86reg
+open x86
+
+definition compute_one_step_noSHA (K : uint32) (W : operand) : code unit :=
+let a := #0, b := #1, c := #2, d := #3, e := #4, f := #5, g := #6, h := #7,
+       a' := #8, b' := #9, c' := #10, d' := #11, e' := #12, f' := #13, g' := #14, h' := #15
+in do
+   eax :== a,
+   b' :== eax,
+   ebx :== b,
+   c' :== ebx,
+   ecx :== c,
+   edx :== ebx,
+
+   ebx &:= eax,
+   edx &:= ecx,
+   ecx &:= eax,
+   ebx ^:= ecx,
+   ebx ^:= ebx,
+
+   -- calculate bsig0
+   ecx :== eax,
+   edx :== eax,
+   ror32 edx 2,
+   ror32 ecx 13,
+   eax ^:= ecx,
+   ror32 edx 22,
+   eax ^:= edx,
+   eax +:= ebx,
+
+   -- calculate my_ch
+   ebx :== e,
+   f' :== ebx,
+   edx :== ebx,
+   not32 edx,
+   ecx :== g,
+   h' :== ecx,
+   edx &:= ecx,
+   ecx :== f,
+   g' :== ecx,
+   ecx &:= ebx,
+   ecx ^:= edx,
+
+   -- calculate bsig1
+   edx :== ebx,
+   edi :== ebx,
+   ror32 edx 6,
+   edx ^:= edi,
+   ror32 ebx 25,
+   edx ^:= ebx,
+
+   -- calculate T1
+   ebx :== h,
+   ebx +:= edx,
+   ebx +:= ecx,
+   ebx +:= K,
+   ecx :== W,
+   ebx +:= ecx,
+   eax +:= ebx,
+   a' :== eax,
+   eax :== d,
+   eax +:= ebx,
+   e' :== eax
+
+
+
+end test_sha256
