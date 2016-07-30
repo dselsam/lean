119303d5c89a343b9393a29bc24af56b08018929
Daniel Selsam <daniel.selsam@gmail.com>
2016-07-30 16:42:41 -0700

    starting rewriting

--- src/library/tactic/simplifier/init_module.cpp
+++ src/library/tactic/simplifier/init_module.cpp
@@ -3,18 +3,19 @@ Copyright (c) 2015 Daniel Selsam. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Daniel Selsam
 */
+#include "library/trace.h"
 #include "library/tactic/simplifier/util.h"
 #include "library/tactic/simplifier/simp_lemmas.h"
-#include "library/tactic/simplifier/rewrite.h"
 #include "library/tactic/simplifier/simp_extensions.h"
 #include "library/tactic/simplifier/simplifier.h"
 
 namespace lean {
 
 void initialize_simplifier_module() {
+    register_trace_class("simplifier");
+
     initialize_simp_util();
     initialize_simp_lemmas();
-    initialize_simp_rewrite();
     initialize_simp_extensions();
     initialize_simplifier();
 }
@@ -22,7 +23,6 @@ void initialize_simplifier_module() {
 void finalize_simplifier_module() {
     finalize_simplifier();
     finalize_simp_extensions();
-    finalize_simp_rewrite();
     finalize_simp_lemmas();
     finalize_simp_util();
 }
