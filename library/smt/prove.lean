fad72d2defbe92825198a50b8f728344c60b62ed
Daniel Selsam <daniel.selsam@gmail.com>
2016-08-02 10:59:17 -0700

    better simp tactic for smt

--- library/smt/prove.lean
+++ library/smt/prove.lean
@@ -1,9 +1,19 @@
 namespace smt
 open tactic
 
+meta_definition intro_all : tactic unit :=
+do t ← target,
+   n ← mk_fresh_name,
+   if expr.is_pi t = tt ∨ expr.is_let t = tt then intro n >> intro_all
+   else return unit.star
+
+lemma true_imp_true [simp] (P : Prop) : (P → true) = true :=
+propext $ and.intro (assume p, trivial) (assume p q, p)
+
 meta_definition prove : tactic unit :=
 do n ← local_context >>= revert_lst,
    simp,
-   now
+   intro_all,
+   assumption
 
 end smt
