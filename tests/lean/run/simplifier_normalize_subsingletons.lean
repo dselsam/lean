4eb0ae489b01252c12b6b0bf817cae758a93f779
Daniel Selsam <daniel.selsam@gmail.com>
2016-08-06 09:50:24 -0700

    normalize ss tests

---
+++ tests/lean/run/simplifier_normalize_subsingletons.lean
@@ -0,0 +1,44 @@
+open tactic
+
+namespace synth_congr
+
+universe variable l
+constants (ss : Π {A : Type.{l}}, A → Type.{l})
+          [ss_ss : ∀ T (t : T), subsingleton (ss t)]
+          (A : Type.{l})
+          (a₁ a₁' : A) (H₁ : a₁ = a₁')
+          (ss₁ : ss a₁)
+          (ss₁' : ss a₁')
+          (f :  Π (X : Type.{l}) (x₁ : X) (ss_x₁ : ss x₁), Type.{l})
+
+attribute ss_ss [instance]
+attribute H₁ [simp]
+
+set_option trace.simplifier.subsingleton true
+example : f A a₁ ss₁ = f A a₁' ss₁' := by simp
+
+end synth_congr
+
+namespace user_congr
+
+universe variable l
+constants (ss : Π {A : Type.{l}}, A → Type.{l})
+          [ss_ss : ∀ T (t : T), subsingleton (ss t)]
+          (A : Type.{l})
+          (a₁ a₁' : A) (H₁ : a₁ = a₁')
+          (ss₁ : ss a₁)
+          (ss₁' : ss a₁')
+          (f :  Π (X : Type.{l}) (x₁ : X) (ss_x₁ : ss x₁), Type.{l})
+          (f_congr : Π (X : Type.{l}) (x₁ x₂ : X) (Hx : x₁ = x₂) (ss₁ : ss x₁), f X x₁ ss₁ = f X x₂ (eq.rec ss₁ Hx))
+
+attribute ss_ss [instance]
+attribute H₁ [simp]
+attribute f_congr [congr]
+
+
+set_option trace.simplifier.subsingleton true
+example : f A a₁ ss₁ = f A a₁' ss₁' := by simp
+
+
+
+end user_congr
