3ee1464fe3e8da35fa02f8910c54066491533dd3
Daniel Selsam <daniel.selsam@gmail.com>
2016-08-12 10:28:59 -0700

    minor

--- src/library/inductive_compiler/init_module.cpp
+++ src/library/inductive_compiler/init_module.cpp
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Author: Daniel Selsam
 */
-#include "library/inductive_compiler.h"
+#include "library/inductive_compiler/init_module.h"
 #include "library/ginductive.h"
 
 namespace lean {
