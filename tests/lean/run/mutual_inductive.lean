fb8179bb1b08746dcfc213fde6e64cd3790b2a14
Daniel Selsam <daniel.selsam@gmail.com>
2016-08-17 14:59:17 -0700

    makers

--- tests/lean/run/mutual_inductive.lean
+++ tests/lean/run/mutual_inductive.lean
@@ -1,11 +1,16 @@
 set_option trace.inductive_compiler.mutual true
 set_option pp.binder_types true
 
-constant (f : ℕ → ℕ → Type)
+universe g
+constant (f : ℕ → ℕ → Type.{g})
 constant (f00 : f 0 0)
 
-constant (g : ℕ → Type)
+constant (g : ℕ → Type.{g})
 constant (g0 : g 0)
+
+set_option pp.all true
+set_option trace.app_builder true
+set_option pp.purify_locals false
 
 mutual_inductive foo₁, foo₂, foo₃, foo₄ (A : Type)
 with foo₁ : Π (n₁ n₂ : nat), f n₁ n₂ → Type
