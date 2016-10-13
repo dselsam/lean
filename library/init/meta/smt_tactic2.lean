10f2a85e28499e228a36917d5eac8ae697470386
Daniel Selsam <daniel.selsam@gmail.com>
2016-10-12 19:46:35 -0700

    everything is an error but basically type checks!

--- library/init/meta/smt_tactic2.lean
+++ library/init/meta/smt_tactic2.lean
@@ -5,10 +5,26 @@ Authors: Daniel Selsam
 -/
 constants (int real : Type)
 
+set_option eqn_compiler.max_steps 1000
+
+namespace Util
+
+private def list.withSpacesCore {X : Type} (f : X -> string) : bool -> list X -> string
+| b [] := ""
+| tt (x::xs) := f x ++ list.withSpacesCore ff xs
+| ff (x::xs) := " " ++ f x ++ list.withSpacesCore ff xs
+
+def list.withSpaces {X : Type} (f : X -> string) : list X -> string := list.withSpacesCore f tt
+
+end Util
+
+open Util
+
 namespace expr
 
 @[reducible] meta def mk_Prop : expr := sort level.zero
 @[reducible] meta def mk_Type : expr := sort (level.succ level.zero)
+@[reducible] meta def mk_Type₂ : expr := sort (level.succ $ level.succ level.zero)
 
 end expr
 
@@ -25,7 +41,14 @@ inductive Sort : Type
 | User : name → list Sort → Sort
 
 namespace Sort
+
 instance : inhabited Sort := ⟨Sort.User `_errorSort []⟩
+
+meta def toSMT : Sort → string
+| Bool        := "Bool"
+| Int         := "Int"
+| Real        := "Real"
+| (User n xs) := "(" ++ n~>to_string ++ " " ++ list.withSpaces toSMT xs ++ ")"
 
 meta def ofExpr : expr → Sort
 | mk_Prop          := Sort.Bool
@@ -46,6 +69,9 @@ namespace SortDecl
 
 instance : inhabited SortDecl := ⟨⟨`_errorSortDecl, 0⟩⟩
 
+meta def toSMT : SortDecl → string
+| (mk n k) := n~>to_string ++ " " ++ k~>to_string
+
 meta def ofExprCore (sortName : name) : expr → nat → SortDecl
 | (pi _ _ mk_Type b)  k := ofExprCore b (succ k)
 | mk_Type             k := ⟨sortName, k⟩
@@ -62,6 +88,9 @@ inductive FunDecl : Type
 namespace FunDecl
 
 instance : inhabited FunDecl := ⟨⟨`_errorFunDecl, [], default Sort⟩⟩
+
+meta def toSMT : FunDecl → string
+| (mk n xs x) := n~>to_string ++ " (" ++ list.withSpaces Sort.toSMT xs ++ ") " ++ Sort.toSMT x
 
 meta def ofExprCore : expr →  list Sort × Sort
 | (pi _ _ dom bod) := match ofExprCore bod with
@@ -83,13 +112,38 @@ namespace Term
 inductive Nullary : Type
 | true, false
 
+meta def Nullary.toSMT : Nullary → string
+| true := "true"
+| false := "false"
+
 inductive Unary : Type
 | not, implies
-| uminus, abs, to_real, to_int, is_int
+| uminus, abs --, to_real, to_int, is_int
+
+meta def Unary.toSMT : Unary → string
+| not     := "not"
+| implies := "=>"
+| uminus  := "-"
+| abs     := "abs"
 
 inductive Binary : Type
 | and, or, eq
 | plus, sub, times, idiv, mod, div, lt, le, gt, ge
+
+meta def Binary.toSMT : Binary → string
+| and     := "and"
+| or      := "or"
+| eq      := "="
+| plus    := "+"
+| sub     := "-"
+| times   := "*"
+| idiv    := "div"
+| mod     := "mod"
+| div     := "/"
+| lt      := "<"
+| le      := "<="
+| gt      := ">"
+| ge      := ">="
 
 end Term
 
@@ -97,8 +151,98 @@ inductive Term : Type
 | nullary : Term.Nullary → Term
 | unary   : Term.Unary → Term → Term
 | binary  : Term.Binary → Term → Term → Term
-| num     : unsigned → Term
-| user    : string → list Term → Term
-| arrow   : Term → Term → Term
+| num     : nat → Term
+| user    : name → list Term → Term
+
+namespace Term
+
+instance : inhabited Term := ⟨user `_errorTerm []⟩
+
+meta def toSMT : Term → string
+| (nullary c)      := c~>toSMT
+| (unary c t)      := "(" ++ c~>toSMT ++ " " ++ toSMT t ++ ")"
+| (binary c t₁ t₂) := "(" ++ c~>toSMT ++ " " ++ toSMT t₁ ++ " " ++ toSMT t₂ ++ ")"
+| (num k)          := k~>to_string
+| (user c ts)      := "(" ++ c~>to_string ++ " " ++ list.withSpaces toSMT ts ++ ")"
+
+meta def ofExpr : expr → Term
+| (const `true [])                  := nullary Nullary.true
+| (const `false [])                 := nullary Nullary.false
+
+| (app (const `not []) e)           := unary Unary.not (ofExpr e)
+| (app (const `implies []) e)       := unary Unary.implies (ofExpr e)
+
+| (app (const `neg []) e)           := unary Unary.uminus (ofExpr e)
+| (app (const `abs []) e)           := unary Unary.abs (ofExpr e)
+
+| (app (const `and [])   (app e₁ e₂)) := binary Binary.and (ofExpr e₁) (ofExpr e₂)
+| (app (const `or [])    (app e₁ e₂)) := binary Binary.or  (ofExpr e₁) (ofExpr e₂)
+| (app (const `eq [])    (app e₁ e₂)) := binary Binary.eq  (ofExpr e₁) (ofExpr e₂)
+
+| (app (const `plus [])  (app e₁ e₂)) := binary Binary.plus (ofExpr e₁) (ofExpr e₂)
+| (app (const `sub [])   (app e₁ e₂)) := binary Binary.sub (ofExpr e₁) (ofExpr e₂)
+| (app (const `times []) (app e₁ e₂)) := binary Binary.times (ofExpr e₁) (ofExpr e₂)
+| (app (const `idiv [])  (app e₁ e₂)) := binary Binary.idiv (ofExpr e₁) (ofExpr e₂)
+| (app (const `mod [])   (app e₁ e₂)) := binary Binary.mod (ofExpr e₁) (ofExpr e₂)
+| (app (const `div [])   (app e₁ e₂)) := binary Binary.div (ofExpr e₁) (ofExpr e₂)
+| (app (const `lt [])    (app e₁ e₂)) := binary Binary.lt (ofExpr e₁) (ofExpr e₂)
+| (app (const `le [])    (app e₁ e₂)) := binary Binary.le (ofExpr e₁) (ofExpr e₂)
+| (app (const `gt [])    (app e₁ e₂)) := binary Binary.gt (ofExpr e₁) (ofExpr e₂)
+| (app (const `ge [])    (app e₁ e₂)) := binary Binary.ge (ofExpr e₁) (ofExpr e₂)
+
+| e := match (get_app_fn e, get_app_args e) with
+       | (const userName [], args) := user userName (list.map ofExpr args)
+       | _                         := default Term
+       end
+
+end Term
+
+inductive Command : Type
+| declareSort : SortDecl → Command
+| declareFun  : FunDecl → Command
+| assert      : Term → Command
+
+namespace Command
+
+meta def toSMT : Command -> string
+| (declareSort sortDecl) := "(declare-sort " ++ sortDecl~>toSMT ++ ")"
+| (declareFun funDecl) := "(declare-fun " ++ funDecl~>toSMT ++ ")"
+| (assert t) := "(assert " ++ t~>toSMT ++ ")"
+
+open tactic
+
+meta def ofHypothesis (hyp : expr) : tactic Command :=
+do hypName ← return $ local_uniq_name hyp,
+   hypType ← infer_type hyp,
+   hypTypeType ← infer_type hypType,
+   match hypTypeType with
+   | mk_Prop  := return $ assert (Term.ofExpr hypType)
+   | mk_Type  := return $ declareFun (FunDecl.ofExpr hypName hypType)
+   | mk_Type₂ := return $ declareSort (SortDecl.ofExpr hypName hypType)
+   | _        := fail $ "unexpected Type of hypothesis: " ++ expr.to_string hypType
+   end
+
+end Command
+
+open tactic monad
+
+meta def goalToCommands : tactic (list Command) :=
+do hyps  ← local_context,
+   tgt   ← target,
+   if not (tgt = expr.const `false [])
+   then fail "expecting goal to be false"
+   else mapM Command.ofHypothesis hyps
+
+example (X : Type) (Y : Type → Type) (x : X) (f g : X → Y int) (H : f x = g x) : false :=
+by do commands ← goalToCommands,
+      trace $ list.withSpaces Command.toSMT commands,
+      failed
+
+
+
+
+
+
+
 
 end smt
