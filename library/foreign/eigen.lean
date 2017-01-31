8105e0ca683ead0cb052db031b4b8d3fae746778
Daniel Selsam <daniel.selsam@gmail.com>
2017-01-31 13:24:52 -0800

    tensors maybe running

--- library/foreign/eigen.lean
+++ library/foreign/eigen.lean
@@ -10,16 +10,20 @@ def tsize : tshape → ℕ
 
 meta constant T : tshape → Type
 
+meta constant to_string {shape : tshape} : T shape → string
+
+meta instance {shape : tshape} : has_to_string (T shape) :=
+has_to_string.mk to_string
+
 meta constant box (α : float) : T []
 
 meta def π : T [] := box float.pi
 
-meta constant const (α : T []) : Π (shape : tshape), T shape
+--meta constant const (α : T []) : Π (shape : tshape), T shape
 --meta constant lift₁ (f : T [] → T []) : Π {shape : tshape}, T shape → T shape
 --meta constant lift₂ (f : T [] → T [] → T []) : Π {shape : tshape}, T shape → T shape → T shape
 
-meta constant zero (shape : tshape) : T shape
-meta constant one (shape : tshape) : T shape
+meta constants zero one : Π (shape : tshape), T shape
 
 meta constants neg inv log exp sqrt tanh : Π {shape : tshape}, T shape → T shape
 meta constants add mul sub div : Π {shape : tshape}, T shape → T shape → T shape
@@ -31,20 +35,19 @@ meta constants sum prod : Π {shape : tshape}, T shape → T []
 meta constant get_row {m n : ℕ} (M : T [m, n]) (ridx : fin m) : T [n]
 meta constant get_col {m n : ℕ} (M : T [m, n]) (cidx : fin n) : T [m]
 
+meta constant get_col_range {m n : ℕ} (M : T [m, n]) (cidx : fin n) (ncols_to_take : ℕ) : T [m, ncols_to_take]
+
 meta constant gemv {m n : ℕ} (M : T [m, n]) (x : T [n]) : T [m]
 meta constant gemm {m n p : ℕ} (M : T [m, n]) (N : T [n, p]) : T [m, p]
 
-meta constant get_col_range {m n : ℕ} (M : T [m, n]) (cidx : fin n) (ncols_to_take : ℕ) : T [m, ncols_to_take]
-
-end T
 end eigen
 
 namespace Test
 open eigen
 
-meta def t₁ : T [2, 2] := T.add (T.one _) (T.one _)
+meta def t₁ : T [2, 2] := eigen.add (eigen.one _) (eigen.one _)
 
 vm_eval t₁
-vm_eval T.gemm t₁ t₁
+vm_eval gemm t₁ t₁
 
 end Test
