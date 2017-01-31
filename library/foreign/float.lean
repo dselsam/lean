9ff9f6d4ce00bfc222b1840a064a9a56a2fb5171
Daniel Selsam <daniel.selsam@gmail.com>
2017-01-31 09:41:52 -0800

    floats basically working

--- library/foreign/float.lean
+++ library/foreign/float.lean
@@ -1,16 +1,27 @@
-constant Float : Type
+-- TODO(dhs): preferably we don't use 'meta' here
+-- instance gave an error, not sure if it is justified
+meta constant float : Type
 
-namespace Float
+namespace float
 
-constants zero one : Float
-constants neg exp log : Float → Float
-constants add mul sub div : Float → Float → Float
+meta constants zero one : float
+meta constants neg inv exp log : float → float
+meta constants add mul sub div : float → float → float
 
-end Float
+meta constant to_string : float → string
+
+meta instance : has_to_string float :=
+has_to_string.mk float.to_string
+
+end float
 
 namespace Test
-open Float
+open float
 
-noncomputable def f₁ : Float := exp (Float.add Float.one Float.one)
--- vm_eval log f₁
+vm_eval float.one
+
+meta def f₁ : float := exp (float.add float.one float.one)
+
+vm_eval log f₁
+
 end Test
