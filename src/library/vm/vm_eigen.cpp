68d7bd5d9c871c8eb8e585ae8f763c61639a8321
Daniel Selsam <daniel.selsam@gmail.com>
2017-03-23 16:30:34 -0700

    minor

--- src/library/vm/vm_eigen.cpp
+++ src/library/vm/vm_eigen.cpp
@@ -3,6 +3,8 @@ Copyright (c) 2017 Daniel Selsam. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author: Daniel Selsam
 */
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <iostream>
 #include <fstream>
 #include <random>
