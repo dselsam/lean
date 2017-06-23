13a6b9e56ef5a1f4caaaa713c0d2919b741f6ff9
Daniel Selsam <daniel.selsam@gmail.com>
2017-06-23 11:58:39 -0700

    network is crashing, switching to home computer

--- src/library/vm/vm_eigen.cpp
+++ src/library/vm/vm_eigen.cpp
@@ -237,6 +237,67 @@ vm_obj eigen_read_from_file(vm_obj const & shape, vm_obj const & _filename, vm_o
     return mk_io_result(to_obj(arr));
 }
 
+static Eigen::ArrayXXf read_mnist_images(string full_path, int& number_of_images, int& image_size) {
+    auto reverseInt = [](int i) {
+        unsigned char c1, c2, c3, c4;
+        c1 = i & 255, c2 = (i >> 8) & 255, c3 = (i >> 16) & 255, c4 = (i >> 24) & 255;
+        return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
+    };
+
+    typedef unsigned char uchar;
+
+    ifstream file(full_path, ios::binary);
+
+    if(file.is_open()) {
+        int magic_number = 0, n_rows = 0, n_cols = 0;
+
+        file.read((char *)&magic_number, sizeof(magic_number));
+        magic_number = reverseInt(magic_number);
+
+        if(magic_number != 2051) throw runtime_error("Invalid MNIST image file!");
+
+        file.read((char *)&number_of_images, sizeof(number_of_images)), number_of_images = reverseInt(number_of_images);
+        file.read((char *)&n_rows, sizeof(n_rows)), n_rows = reverseInt(n_rows);
+        file.read((char *)&n_cols, sizeof(n_cols)), n_cols = reverseInt(n_cols);
+
+        image_size = n_rows * n_cols;
+
+        Eigen::ArrayXXf mat(number_of_images, image_size_images);
+
+        uchar** _dataset = new uchar*[number_of_images];
+        for(int i = 0; i < number_of_images; i++) {
+            uchar* image = new uchar[image_size];
+            file.read((char *)_dataset[i], image_size);
+        }
+        return _dataset;
+    } else {
+        throw runtime_error("Cannot open file `" + full_path + "`!");
+    }
+}
+
+vm_obj eigen_read_mnist(vm_obj const & shape, vm_obj const & _filename, vm_obj const &, vm_obj const &) {
+    std::string filename = to_string(_filename);
+    std::ifstream in(filename);
+
+    unsigned nrows, ncols;
+    if (optional<pair<unsigned, unsigned> > mn = is_matrix(shape)) {
+        nrows = mn->first;
+        ncols = mn->second;
+    } else {
+        nrows = shape_len(shape);
+        ncols = 1;
+    }
+    Eigen::ArrayXXf arr(nrows, ncols);
+
+    for (unsigned row = 0; row < nrows; row++) {
+        for (unsigned col = 0; col < ncols; col++) {
+            in >> arr(row, col);
+        }
+    }
+    return mk_io_result(to_obj(arr));
+}
+
+
 vm_obj eigen_write_to_file(vm_obj const & /* shape */, vm_obj const & x, vm_obj const & _filename, vm_obj const &, vm_obj const &) {
     std::string filename = to_string(_filename);
     std::ofstream out(filename);
